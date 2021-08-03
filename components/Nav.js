d15736710bbccbcf2625b28770d49f6c6c0b2de3
roboRiot <igor.a.sav@gmail.com>
2021-08-03 14:24:41 -0700

    still working on the home page

--- components/Nav.js
+++ components/Nav.js
@@ -46,10 +46,10 @@ const Navigate = () => {
             </a>
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="#" class="nav-link px-2 text-white">Featured</a></li>
+              <li><a href="/" class="nav-link px-2 text-white">Featured</a></li>
               <li><a href="#" class="nav-link px-2 text-white">Browse</a></li>
               <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-              <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+              <li><a href="/about" class="nav-link px-2 text-white">About</a></li>
             </ul>
 
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
