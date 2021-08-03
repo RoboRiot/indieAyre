f3314b7d3c5940352a9bd3c8d4909a42a2300669
roboRiot <igor.a.sav@gmail.com>
2021-08-02 20:11:37 -0700

    fixed navbar and added carousel

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Nav from './Nav'
 import Meta from './Meta'
-import Header from './Header'
+import Home from './Home'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
@@ -8,12 +8,15 @@ const Layout = ({ children }) => {
     <>
       <Meta />
       <Nav />
-      <div className={styles.container}>
+      <Home />
+      <div className={styles.browse}></div>
+      {/* <div className={styles.container}>
+      
         <main className={styles.main}>
-          <Header />
+          
           {children}
         </main>
-      </div>
+      </div> */}
     </>
   )
 }
